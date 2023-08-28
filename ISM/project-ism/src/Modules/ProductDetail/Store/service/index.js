1b7cb81132c7d94701059c95fa41ca4ee9345c13
Lê Thanh Phương Nam <75212572+NamBobby@users.noreply.github.com>
2023-08-29 00:37:18 +0700

    Add files via upload

---
+++ ISM/project-ism/src/Modules/ProductDetail/Store/service/index.js
@@ -0,0 +1,22 @@
+import API from "../../configs/api";
+import apiMethod from "@utility/ApiMethod";
+
+export const getDetailProduct = (payload) => {
+  const { id } = payload || {};
+  return apiMethod.get(API.GET_DETAIL_PRODUCT + `/${id}`);
+};
+
+export const createCart = (payload) => {
+  const { userId, cartProduct } = payload || {};
+  return apiMethod.post(API.CREATE_CART, { userId, cartProduct });
+};
+
+export const getListCart = (payload) => {
+  // console.log("payload", payload);
+  const { userId } = payload || {};
+  return apiMethod.post(API.GET_LIST_CART, { userId });
+};
+
+export const deleteItemCart = (payload) => {
+  return apiMethod.post(API.DELETE_ITEM_CART, payload);
+};
---
+++ ISM/project-ism/src/Modules/ProductDetail/configs/api.js
@@ -0,0 +1,6 @@
+export default {
+  GET_DETAIL_PRODUCT: "/products/detail",
+  CREATE_CART: "/cart/create-cart",
+  GET_LIST_CART: "/cart/get-cart",
+  DELETE_ITEM_CART: "/cart/delete-item",
+};
